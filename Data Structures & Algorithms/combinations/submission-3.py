860f738c6c938b1df883c4f9ea31fc23c6b4c0fc
Anika3689 <130129455+Anika3689@users.noreply.github.com>
2026-03-30 21:01:56 -0700

    Add: combinations - submission-3

---
+++ Data Structures & Algorithms/combinations/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        def enumerateK(curCombo: list, start: int, used: set, i: int):
+            nonlocal combinations, end
+            if i > k:
+                combinations.append(curCombo.copy())
+                return
+
+            for choice in range(start, end + 1):
+                curCombo.append(choice)
+                enumerateK(curCombo, choice + 1, used, i + 1)
+                curCombo.pop()
+
+
+        combinations = []
+        start, end = 1, n
+        used = set()
+        curCombo = []
+        enumerateK(curCombo, start, used, 1)
+        return combinations
+        
+        
