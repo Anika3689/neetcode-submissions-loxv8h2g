54fa4af7ee221cd942440d7521b0fcf67fd640fc
Anika3689 <130129455+Anika3689@users.noreply.github.com>
2026-04-01 16:10:59 -0700

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        allPerms = []
+
+        def helper(curPerm, counts):
+            if len(curPerm) == len(nums):
+                allPerms.append(curPerm.copy())
+            
+            for num in counts:
+                if counts[num] <= 0:
+                    continue
+                curPerm.append(num)
+                counts[num] -= 1
+                helper(curPerm, counts)
+                curPerm.pop()
+                counts[num] += 1
+
+        curPerm = []
+        counts = Counter(nums)
+        helper(curPerm, counts)
+        return allPerms
