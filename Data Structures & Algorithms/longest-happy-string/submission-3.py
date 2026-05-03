0427da63c0d57614d8c9bceb8e848ecb4d036c21
Anika3689 <130129455+Anika3689@users.noreply.github.com>
2026-05-02 21:46:01 -0700

    Add: longest-happy-string - submission-3

---
+++ Data Structures & Algorithms/longest-happy-string/submission-3.py
@@ -0,0 +1,44 @@
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        remainingCounts = []
+        if a > 0:
+            remainingCounts.append((-a, 'a'))
+        if b > 0:
+            remainingCounts.append((-b, 'b'))
+        if c > 0:
+            remainingCounts.append((-c, 'c'))
+
+        heapq.heapify(remainingCounts)
+        res = []
+        cooldown = []
+
+        while cooldown or remainingCounts:
+            removeFromCooldown = set()
+            for i, item in enumerate(cooldown):
+                letter, remaining, wait = item
+                if wait == 0:
+                    heapq.heappush(remainingCounts, (remaining, letter))
+                    removeFromCooldown.add(i)
+                else:
+                    cooldown[i][-1] -= 1
+
+            for i in removeFromCooldown:
+                cooldown.pop(i)
+
+            if not remainingCounts:
+                break
+            remainingOccurences, mostRemainingLetter = heapq.heappop(remainingCounts)
+            res.append(mostRemainingLetter)
+            if remainingOccurences + 1 == 0:
+                continue
+
+            if len(res) > 1 and res[-2] == res[-1]:
+                cooldown.append([mostRemainingLetter, remainingOccurences + 1, 1])
+            else:
+                heapq.heappush(remainingCounts, (remainingOccurences + 1, mostRemainingLetter))
+            
+
+        return ''.join(res)
+
+
+
