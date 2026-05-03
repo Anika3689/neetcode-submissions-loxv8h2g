d3df10819de33602abf4a472e5c073e47d0b18a5
Anika3689 <130129455+Anika3689@users.noreply.github.com>
2026-05-03 15:48:52 -0700

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.py
@@ -0,0 +1,31 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.lowerHalf = [] 
+        self.upperHalf = []
+
+    def rebalance_halves(self) -> None:
+        while len(self.lowerHalf) - len(self.upperHalf) > 1:
+            heapq.heappush(self.upperHalf, heapq.heappop_max(self.lowerHalf))
+        while len(self.upperHalf) - len(self.lowerHalf) > 1:
+            heapq.heappush_max(self.lowerHalf, heapq.heappop(self.upperHalf))
+
+    def addNum(self, num: int) -> None:
+        if self.upperHalf and num >= self.upperHalf[0]:
+            heapq.heappush(self.upperHalf, num)
+            self.rebalance_halves()
+        else:
+            heapq.heappush_max(self.lowerHalf, num)
+            self.rebalance_halves()
+        
+        print(self.lowerHalf, self.upperHalf)
+
+    def findMedian(self) -> float:
+        if len(self.lowerHalf) == len(self.upperHalf):
+            return (self.lowerHalf[0] + self.upperHalf[0]) / 2
+        elif len(self.lowerHalf) > len(self.upperHalf):
+            return self.lowerHalf[0]
+        else:
+            return self.upperHalf[0]
+
+        
