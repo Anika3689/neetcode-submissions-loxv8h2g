1327a59173b644e0bc36c30f774616bd4e983116
Anika3689 <130129455+Anika3689@users.noreply.github.com>
2026-05-19 16:40:36 -0700

    Add: islands-and-treasure - submission-4

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        m = len(grid)
+        n = len(grid[0])
+
+        queue = deque()
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    queue.append((i, j, 0))
+
+        INF = 2147483647
+        while queue:
+            i, j, dist = queue.popleft()
+            if grid[i][j] == INF or grid[i][j] == 0:
+                grid[i][j] = dist
+            else:
+                continue
+
+            if i + 1 < m and grid[i + 1][j] == INF:
+                queue.append((i + 1, j, dist + 1))
+            if i - 1 >= 0 and grid[i - 1][j] == INF:
+                queue.append((i - 1, j, dist + 1))
+            if j + 1 < n and grid[i][j + 1] == INF:
+                queue.append((i, j + 1, dist + 1))
+            if j - 1 >= 0 and grid[i][j - 1] == INF:
+                queue.append((i, j - 1, dist + 1))
+
+
+
+
+        
